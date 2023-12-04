0d0a1aa418e5e3731a8764102c2092627eaa0721
marcosherif <68437948+marcosherif@users.noreply.github.com>
2023-12-04 03:14:29 +0200

    initial gui

--- Cryptography.py
+++ Cryptography.py
@@ -6,37 +6,38 @@ from IVError import IVError
 import os
 import base64
 
-def encryptAESFile(filename,key,mode="ECB",IV="0000000000000000"):
+
+def encryptAESFile(filename, key, mode="ECB", IV="0000000000000000"):
     chunksize = 16
-    outputFile = "(enc)"+filename
+    outputFile = "(enc)" + filename
     filesize = str(os.path.getsize(filename)).zfill(16)
-    with open(filename, 'rb') as infile:#rb means read in binary
-        with open(outputFile, 'wb') as outfile:#wb means write in the binary mode
-            if(mode=="CBC"):
+    with open(filename, 'rb') as infile:  # rb means read in binary
+        with open(outputFile, 'wb') as outfile:  # wb means write in the binary mode
+            if (mode == "CBC"):
                 outfile.write(filesize.encode('utf-8'))
                 while True:
                     chunk = infile.read(chunksize)
                     if not chunk:
                         break  # Break out of the loop when the end of the file is reached
-                    outfile.write(encryptAESText(chunk.replace('\n',''),key,"CBC",IV))
+                    outfile.write(encryptAESText(chunk.replace('\n', ''), key, "CBC", IV))
                     outfile.write(b'\n')
-            elif(mode=="ECB"):
+            elif (mode == "ECB"):
                 outfile.write(filesize.encode('utf-8'))
                 outfile.write('\n')
                 while True:
                     chunk = infile.read(chunksize)
                     if not chunk:
-                        break 
-                    outfile.write(encryptAESText(chunk.replace('\n',''),key))
+                        break
+                    outfile.write(encryptAESText(chunk.replace('\n', ''), key))
                     outfile.write('\n')
-                
-                    
-def decryptAESFile(filename,key,mode="ECB"):
+
+
+def decryptAESFile(filename, key, mode="ECB"):
     chunksize = 16
-    outputFile = filename.replace('(enc)','')
+    outputFile = filename.replace('(enc)', '')
     # outputFile = filename.replace('(dec)','')
     with open(filename, 'rb') as infile:
-        if(mode=="CBC"):
+        if (mode == "CBC"):
             filesize = int(infile.read(16))
             IV = infile.read(16)
             with open(outputFile, 'wb') as outfile:
@@ -44,27 +45,27 @@ def decryptAESFile(filename,key,mode="ECB"):
                     chunk = infile.read(chunksize)
                     if len(chunk) == 0:
                         break
-                    outfile.write(decryptAESText(chunk,key,"CBC",IV).decode)
+                    outfile.write(decryptAESText(chunk, key, "CBC", IV).decode)
                 outfile.truncate(filesize)
-                    
-        elif(mode=="ECB"):
+
+        elif (mode == "ECB"):
             filesize = int(infile.read(16))
             with open(outputFile, 'wb') as outfile:
                 while True:
                     chunk = infile.read(chunksize)
                     if len(chunk) == 0:
                         break
-                    outfile.write(decryptAESText(chunk,key,"ECB"))
+                    outfile.write(decryptAESText(chunk, key, "ECB"))
                 outfile.truncate(filesize)
 
 
-def encryptAESText(plaintext, key,mode="ECB",IV="0000000000000000"):
-    if(len(key)!=16 and len(key)!=24 and len(key)!=32):
-        raise  KeyError("Key must have a fixed size [16,24,32]")
-    if(mode=="ECB"):
+def encryptAESText(plaintext, key, mode="ECB", IV="0000000000000000"):
+    if (len(key) != 16 and len(key) != 24 and len(key) != 32):
+        raise KeyError("Key must have a fixed size [16,24,32]")
+    if (mode == "ECB"):
         cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
-    elif mode=="CBC":
-        if(len(IV)<16):
+    elif mode == "CBC":
+        if (len(IV) < 16):
             raise IVError("IV must be 16 bytes long")
         cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
     encryptor = cipher.encryptor()
@@ -74,13 +75,14 @@ def encryptAESText(plaintext, key,mode="ECB",IV="0000000000000000"):
     encodedciphertext = base64.b64encode(ciphertext)
     return encodedciphertext
 
-def decryptAESText(ciphertext, key,mode="ECB",IV="0000000000000000"):
-    if(len(key)!=16 and len(key)!=24 and len(key)!=32):
-        raise  KeyError("Key must have a fixed size [16,24,32]")
-    if(mode=="ECB"):
+
+def decryptAESText(ciphertext, key, mode="ECB", IV="0000000000000000"):
+    if (len(key) != 16 and len(key) != 24 and len(key) != 32):
+        raise KeyError("Key must have a fixed size [16,24,32]")
+    if (mode == "ECB"):
         cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
-    elif (mode=="CBC"):
-        if(len(IV)<16):
+    elif (mode == "CBC"):
+        if (len(IV) < 16):
             raise IVError("IV must be 16 bytes long")
         cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
     decryptor = cipher.decryptor()
@@ -88,15 +90,16 @@ def decryptAESText(ciphertext, key,mode="ECB",IV="0000000000000000"):
     padded_data = decryptor.update(decodedciphertext) + decryptor.finalize()
     unpadder = PKCS7(algorithms.AES.block_size).unpadder()
     plaintext = unpadder.update(padded_data) + unpadder.finalize()
-    return plaintext        
+    return plaintext
+
 
 def main():
-    key=b"PhiloPteer Mohebmmmmmmmmmmmmmmmm"
+    key = b"PhiloPteer Mohebmmmmmmmmmmmmmmmm"
     # choice = input("Would you like to (E)encrypt or (D)Decrypt ")
     print(len(key))
     # plaintext = input("Enter the plaintext: ").encode()
     # # iv= b'\x00' * (algorithms.AES.block_size // 8)
-    iv=b"0000000000000000"
+    iv = b"0000000000000000"
     print(iv)
     print(len(iv))
     # enc = encryptAESText(plaintext, key,"CBC",iv)
@@ -107,44 +110,45 @@ def main():
     if choice == 'E':
         choice = input("Text(T) or File(F) to encrypt: ")
         key = input("Key: ").encode()
-        if(choice=="T"):
-            choice=input("Mode(ECB,CBC): ")
-            plaintext=input("Text: ").encode()
-            if(choice=="ECB"):
-                print(encryptAESText(plaintext,key))
-            elif(choice=="CBC"):
-                IV=input("IV: ").encode()
-                print(encryptAESText(plaintext,key,"CBC",IV))
-        elif(choice=="F"):
-            choice=input("Mode(ECB,CBC): ")
-            filename=input("Filename: ")
-            if(choice=="ECB"):
-                print(encryptAESFile(filename,key))
-            elif(choice=="CBC"):
-                IV=input("IV: ").encode()
-                print(encryptAESFile(filename,key,"CBC",IV))
+        if (choice == "T"):
+            choice = input("Mode(ECB,CBC): ")
+            plaintext = input("Text: ").encode()
+            if (choice == "ECB"):
+                print(encryptAESText(plaintext, key))
+            elif (choice == "CBC"):
+                IV = input("IV: ").encode()
+                print(encryptAESText(plaintext, key, "CBC", IV))
+        elif (choice == "F"):
+            choice = input("Mode(ECB,CBC): ")
+            filename = input("Filename: ")
+            if (choice == "ECB"):
+                print(encryptAESFile(filename, key))
+            elif (choice == "CBC"):
+                IV = input("IV: ").encode()
+                print(encryptAESFile(filename, key, "CBC", IV))
         print('Done.')
     elif choice == 'D':
         choice = input("Text(T) or File(F) to Decrypt: ")
         key = input("Key: ").encode()
-        if(choice=="T"):
-            choice=input("Mode(ECB,CBC): ")
-            ciphertext=input("Text: ").encode()
-            if(choice=="ECB"):
-                print(decryptAESText(ciphertext,key))
-            elif(choice=="CBC"):
-                IV=input("IV: ").encode()
-                print(decryptAESText(plaintext,key,"CBC",IV))
-        elif(choice=="F"):
-            choice=input("Mode(ECB,CBC): ")
-            filename=input("Filename: ")
-            if(choice=="ECB"):
-                print(dcryptAESFile(filename,key))
-            elif(choice=="CBC"):
-                IV=input("IV: ").encode()
-                print(decryptAESFile(filename,key,"CBC"))
+        if (choice == "T"):
+            choice = input("Mode(ECB,CBC): ")
+            ciphertext = input("Text: ").encode()
+            if (choice == "ECB"):
+                print(decryptAESText(ciphertext, key))
+            elif (choice == "CBC"):
+                IV = input("IV: ").encode()
+                print(decryptAESText(plaintext, key, "CBC", IV))
+        elif (choice == "F"):
+            choice = input("Mode(ECB,CBC): ")
+            filename = input("Filename: ")
+            if (choice == "ECB"):
+                print(decryptAESFile(filename, key))
+            elif (choice == "CBC"):
+                IV = input("IV: ").encode()
+                print(decryptAESFile(filename, key, "CBC"))
         print('Done.')
     else:
         print("No option selected, closing...")
-    
+
+
 main()
---
+++ gui.py
@@ -0,0 +1,52 @@
+from tkinter import *
+
+root = Tk()
+
+root.title('Crypt-It')
+
+# options frame and its constituents
+options_frame = LabelFrame(root, text="Choose your options:", padx=20, pady=50)
+options_frame.grid(row=0, column=0)
+
+options = ["AES", "RSA", "sha512"]
+
+clicked = StringVar()
+clicked.set("Choose Tool")
+
+drop = OptionMenu(options_frame, clicked, *options)
+drop.config(width=60)
+drop.grid(row=0, column=0, columnspan=10)
+
+key_field = Text(options_frame, width=50, height=15)
+key_field.insert(END, "Enter Your Key")
+key_field.grid(row=1, column=0, columnspan=10, pady=10)
+
+en_button = Button(options_frame, text="ENCRYPT", pady=10, padx=20, width=22)
+en_button.grid(row=7, column=0, padx=4)
+
+dec_button = Button(options_frame, text="DECRYPT", pady=10, padx=20, width=22)
+dec_button.grid(row=7, column=2, padx=4)
+
+# right frame
+r_frame = LabelFrame(root, borderwidth=0, highlightthickness=0)
+r_frame.grid(row=0, column=1)
+
+# input frame and its constituents
+in_frame = LabelFrame(r_frame, text="INPUT TEXT", padx=10, pady=10)
+in_frame.pack(pady=10)
+
+input_text = Text(in_frame, width=50, height=10)
+input_text.insert(END, "Enter Text")
+input_text.grid(row=0, column=0, pady=5)
+
+
+# output frame and its constituents
+out_frame = LabelFrame(r_frame, text="OUTPUT TEXT", padx=10, pady=10)
+out_frame.pack(pady=10)
+
+output_text = Text(out_frame, width=50, height=10)
+output_text.insert(END, "Result")
+output_text.config(state="disabled")
+output_text.grid(row=0, column=0, pady=5)
+
+root.mainloop()
